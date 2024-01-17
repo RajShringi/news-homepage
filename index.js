0937685221eb827d24a22be748c727214fe38c7d
raj shringi <raj24shringi@gmail.com>
2024-01-17 16:31:46 +0530

    create functions to simplify code

--- index.js
+++ index.js
@@ -8,20 +8,28 @@ hamburger.addEventListener("click", () => {
   if (mobileNav.classList.contains("hidden")) {
     mobileNav.classList.add("flex", "slideIn");
     mobileNav.classList.remove("hidden");
-    hamburger.classList.add("fixed", "right-[5%]");
-    middleLine.classList.add("hidden");
-    topLine.setAttribute("y", "45");
-    bottomLine.setAttribute("y", "45");
-    topLine.classList.add("rotate-45");
-    bottomLine.classList.add("-rotate-45");
-  } else {
-    mobileNav.classList.add("hidden");
-    mobileNav.classList.remove("flex", "slideIn");
-    middleLine.classList.remove("hidden");
-    hamburger.classList.remove("fixed", "right-[5%]");
-    topLine.setAttribute("y", "25");
-    bottomLine.setAttribute("y", "65");
-    topLine.classList.remove("rotate-45");
-    bottomLine.classList.remove("-rotate-45");
+    changeHamburgerToCross();
+    return;
   }
+  mobileNav.classList.add("hidden");
+  mobileNav.classList.remove("flex", "slideIn");
+  changeCrossToHamburger();
 });
+
+function changeHamburgerToCross() {
+  hamburger.classList.add("fixed", "right-[5%]");
+  middleLine.classList.add("hidden");
+  topLine.setAttribute("y", "45");
+  bottomLine.setAttribute("y", "45");
+  topLine.classList.add("rotate-45");
+  bottomLine.classList.add("-rotate-45");
+}
+
+function changeCrossToHamburger() {
+  hamburger.classList.remove("fixed", "right-[5%]");
+  middleLine.classList.remove("hidden");
+  topLine.setAttribute("y", "25");
+  bottomLine.setAttribute("y", "65");
+  topLine.classList.remove("rotate-45");
+  bottomLine.classList.remove("-rotate-45");
+}
